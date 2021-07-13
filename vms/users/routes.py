04aa12fb3ddc4a43e5fc3491eced8a1df46bb1f4
funix <beeautoscript@gmail.com>
2021-07-13 16:51:20 +0530

    Vehicle Managment System

--- vms/users/routes.py
+++ vms/users/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,render_template,url_for, flash, redirect, request, abort, session
 from vms import app,db,login_manager,bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
-from vms.models import Users
+from vms.models import Users,RegisteredVehicle
 
 
 # Blueprint object
@@ -11,6 +11,7 @@ blue = Blueprint('users',__name__,template_folder='templates')
 # User Home
 @blue.route('/user/home',methods=['GET','POST'])
 def home():
+    
     return render_template('users/home.html',title='Home')
 
 
