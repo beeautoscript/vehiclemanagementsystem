0024e030e621fc727baf8a29f93b986ee5200377
funix <beeautoscript@gmail.com>
2021-07-24 21:04:24 +0530

    Vehicle Management System

--- vms/vms_entry_api/routes.py
+++ vms/vms_entry_api/routes.py
@@ -2,7 +2,7 @@ import json
 from flask import Blueprint,render_template,url_for, flash, redirect, request, abort, session
 from vms import app,db,login_manager,bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
-from vms.models import Users,RegisteredVehicle,VehicleOnPremises
+from vms.models import Users,RegisteredVehicle,VehicleOnPremises,Entryexitime
 
 
 # Blueprint object
@@ -12,33 +12,59 @@ blue = Blueprint('vms_entry_api',__name__,template_folder='templates')
 @blue.route('/vms/api/v.1.0/entry',methods=['POST'])
 def vmsentry():
     data = request.get_json()
-    # check if tagid is already available in database
-    if len(VehicleOnPremises.query.filter(VehicleOnPremises.tagid == data[0]['tagid']).all()) == 0:
+
+    # check if tagid is already available in database : VehicleOnPremises DB
+    if len(VehicleOnPremises.query.filter(VehicleOnPremises.tagid == data[0]['epc_id']).all()) == 0:
         # new tag vehicle entered premises
         # add new tag entry in database
-        new_tag_entry = VehicleOnPremises(tagid=data[0]['tagid'],entrytime=data[0]['timestamp'],status=True)
+        new_tag_entry = VehicleOnPremises(tagid=data[0]['epc_id'],entrytime=data[0]['timestamp'],status=True)
         db.session.add(new_tag_entry)
         db.session.commit()
-        return json.dumps({'result':'successfully_added_new_tag','status':200})
     else:
         # update entry time of on-premise tagged vehicle
-        kwargs = {'tagid':str(data[0]['tagid'])}
+        kwargs = {'tagid':str(data[0]['epc_id'])}
         vehicle_table_id = VehicleOnPremises.query.filter_by(**kwargs).first()
         vehicle_table_id.entrytime = str(data[0]['timestamp'])
         vehicle_table_id.status = True
         db.session.commit()
-        return json.dumps({'result':'successfully_updated_entry_data','status':200})
+
+    # check if tag is registered,if yes then add the entry time : Entryexitime DB
+    if len(RegisteredVehicle.query.filter(RegisteredVehicle.tagid == data[0]['epc_id']).all()) != 0:
+        kwargs = {'tagid':str(data[0]['epc_id'])}
+        registered_vehicle_table_id = RegisteredVehicle.query.filter_by(**kwargs).first()
+        #add entry time
+        add_entry_time = Entryexitime(entrytime=str(data[0]['timestamp']),registeredvehicle_id=registered_vehicle_table_id.id)
+        db.session.add(add_entry_time)
+        db.session.commit()
+        return json.dumps({'result':'entry_time_entryexit_db','status':200})
+    else:
+        return json.dumps({'result':'entry_time_vehicleonpremises_db','status':200})
+
+
 
 # Vehicle Exit
 @blue.route('/vms/api/v.1.0/exit',methods=['POST'])
 def vmsexit():
     data = request.get_json()
-    # update exit time and entry-exit boolean
-    kwargs = {'tagid':str(data[0]['tagid'])}
-    vehicle_table_id = VehicleOnPremises.query.filter_by(**kwargs).first()
-    vehicle_table_id.exitime = str(data[0]['timestamp'])
-    vehicle_table_id.status = False
-    db.session.commit()
-    return json.dumps({'result':'successfully_updated_exit_data','status':200})
 
+    # check if tagid is already available in database : VehicleOnPremises DB
+    if len(VehicleOnPremises.query.filter(VehicleOnPremises.tagid == data[0]['epc_id']).all()) != 0:
+        # update exit time of on-premise tagged vehicle
+        kwargs = {'tagid':str(data[0]['epc_id'])}
+        vehicle_table_id = VehicleOnPremises.query.filter_by(**kwargs).first()
+        vehicle_table_id.exitime = str(data[0]['timestamp'])
+        vehicle_table_id.status = False
+        db.session.commit()
+
+    # check if tag is registered,if yes then add the entry time : Entryexitime DB
+    if len(RegisteredVehicle.query.filter(RegisteredVehicle.tagid == data[0]['epc_id']).all()) != 0:
+        kwargs = {'tagid':str(data[0]['epc_id'])}
+        registered_vehicle_table_id = RegisteredVehicle.query.filter_by(**kwargs).first()
+        #add entry time
+        add_entry_time = Entryexitime(exitime=str(data[0]['timestamp']),registeredvehicle_id=registered_vehicle_table_id.id)
+        db.session.add(add_entry_time)
+        db.session.commit()
+        return json.dumps({'result':'entry_time_entryexit_db','status':200})
+    else:
+        return json.dumps({'result':'entry_time_vehicleonpremises_db','status':200})
 
