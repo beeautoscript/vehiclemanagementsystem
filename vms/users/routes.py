f9514ee3af01c08677cf90b14a3ba8fc6fb2840c
funix <beeautoscript@gmail.com>
2021-07-27 02:56:55 +0530

    Vehicle Management System

--- vms/users/routes.py
+++ vms/users/routes.py
@@ -3,8 +3,9 @@ from vms import app,db,login_manager,bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 from vms.models import Users,RegisteredVehicle,VehicleOnPremises
 import datetime
-from datetime import timedelta
-from datetime import datetime
+# from datetime import datetime
+# from datetime import timedelta
+
 
 # Blueprint object
 blue = Blueprint('users',__name__,template_folder='templates')
@@ -18,23 +19,30 @@ def home():
     tagged_vehicles = len(RegisteredVehicle.query.filter(RegisteredVehicle.user_id==current_user.id,RegisteredVehicle.tagid != None).all())
     vehicle_inside_premises = len(VehicleOnPremises.query.filter(VehicleOnPremises.status != False).all())
     vehicle_exited_premises = len(VehicleOnPremises.query.filter(VehicleOnPremises.status != True).all())
-    registered_vehicle = len(VehicleOnPremises.query.filter(VehicleOnPremises.registered != True).all())
-
+    
     # average time of vehicles inside premises
     if vehicle_inside_premises == 0:
         average_time = 0
     else:
-        total_vehicle_on_premises_query = VehicleOnPremises.query.all()
-        total_time_inside_premises_list = [ i.entrytime.split(' ')[1] for i in  total_vehicle_on_premises_query]
-        average_time = str(timedelta(seconds=sum(map(lambda f: int(f[0])*3600 + int(f[1])*60 + int(f[2]),map(lambda f: f.split(':'),total_time_inside_premises_list)))/len(total_time_inside_premises_list)))
-        # average_time_split = average_time.split(':')
-        # average_time_in_minutes = int(average_time_split[0])*60 + int(average_time_split[1])*1 + int(average_time_split[2])/60
-        # hours = int(average_time_in_minutes)
-        # minutes = (average_time_in_minutes*60) % 60
-        # seconds = (average_time_in_minutes*3600) % 60
-        # avrerage_time = ("%d:%02d.%02d" % (hours, minutes, seconds))
+        average_time_list = []
+        total_avergae_time = 0
+        query_db = db.session.query(VehicleOnPremises).filter(VehicleOnPremises.entrytime != None,VehicleOnPremises.exitime != None).all()
+        for i in query_db:
+            exitime = i.exitime.split(' ')[1]+" "+i.exitime.split(' ')[2]
+            entrytime = i.entrytime.split(' ')[1]+" "+i.entrytime.split(' ')[2]
+            exitformat = datetime.datetime.strptime(exitime,'%I:%M:%S %p')
+            entryformat = datetime.datetime.strptime(entrytime,'%I:%M:%S %p')
+            td = exitformat - entryformat
+            average_time_list.append(int(td.total_seconds()))
 
-    return render_template('users/home.html',title='Home',count_untagged=untagged_vehicles,count_tagged=tagged_vehicles,count_vehicle_inside_premises=vehicle_inside_premises,count_vehicle_exit_premises=vehicle_exited_premises,average_time=average_time,registered_vehicle=registered_vehicle)
+        for i in average_time_list:
+            total_avergae_time += i
+
+        total_seconds = int(total_avergae_time/len(average_time_list))
+        average_time_format = datetime.timedelta(seconds=total_seconds)
+        average_time=str(average_time_format)
+
+    return render_template('users/home.html',title='Home',count_untagged=untagged_vehicles,count_tagged=tagged_vehicles,count_vehicle_inside_premises=vehicle_inside_premises,count_vehicle_exit_premises=vehicle_exited_premises,average_time=average_time)
 
 # Total Vehicle inside premises
 @blue.route('/user/onpremises',methods=['GET','POST'])
